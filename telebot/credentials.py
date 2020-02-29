ac906aaec9e3f16a59db8349b7a980ac6dca199a
Steven Chia <stevenchia56@gmail.com>
2020-02-29 21:43:36 +0800

    second commit

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = '1116205707:AAHxdH5GKgzHhbGmxi8ez3IiIbOTYr1rftE'
-bot_user_name = '@@is_your_friend_crazy_bot'
+bot_user_name = '@is_your_friend_crazy_bot'
 URL = 'https://is-your-friend-crazy.herokuapp.com/'
